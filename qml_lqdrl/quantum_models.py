bc18697adb260f722e02f0a1d40a07014532e988
piersk <piers.keegan@ucdconnect.ie>
2025-07-14 15:32:34 +0100

    Migrating the actor-critic network to a separate file with its own critic and actor classes

---
+++ qml_lqdrl/quantum_models.py
@@ -0,0 +1,50 @@
+import pennylane as qml
+from pennylane import numpy as np
+
+class QuantumActor:
+    def __init__(self, n_qubits, m_layers):
+        self.n_qubits = n_qubits
+        self.m_layers = m_layers
+        self.dev = qml.device("default.qubit", wires=n_qubits)
+        self.theta = np.random.randn(m_layers, n_qubits, requires_grad=True)
+
+        @qml.qnode(self.dev, interface="autograd")
+        def circuit(x, theta):
+            for i in range(n_qubits):
+                qml.Hadamard(wires=i)
+                qml.RX(x[i], wires=i)
+            for l in range(m_layers):
+                for i in range(n_qubits):
+                    qml.RY(theta[l][i], wires=i)
+                for i in range(n_qubits - 1):
+                    qml.CZ(wires=[i, i+1])
+            return [qml.expval(qml.PauliZ(i)) for i in range(n_qubits)]
+
+        self.qnode = circuit
+
+    def __call__(self, x):
+        return self.qnode(x, self.theta)
+
+class QuantumCritic:
+    def __init__(self, n_qubits, m_layers):
+        self.n_qubits = n_qubits
+        self.m_layers = m_layers
+        self.dev = qml.device("default.qubit", wires=n_qubits)
+        self.theta = np.random.randn(m_layers, n_qubits, requires_grad=True)
+
+        @qml.qnode(self.dev, interface="autograd")
+        def circuit(x, theta):
+            for i in range(n_qubits):
+                qml.Hadamard(wires=i)
+                qml.RX(x[i], wires=i)
+            for l in range(m_layers):
+                for i in range(n_qubits):
+                    qml.RY(theta[l][i], wires=i)
+                for i in range(n_qubits - 1):
+                    qml.CZ(wires=[i, i+1])
+            return qml.expval(qml.PauliZ(0))
+
+        self.qnode = circuit
+
+    def __call__(self, x):
+        return self.qnode(x, self.theta)
