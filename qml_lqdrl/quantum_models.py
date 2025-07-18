057b09e4f12961421f5c4fd8d91773a1e6e145f4
piersk <piers.keegan@ucdconnect.ie>
2025-07-18 13:37:26 +0100

    Updated quantum model to measure all qubits in Z-basis for actor-critic network. Updated decoding operation to return a scalar from a tensor object

--- qml_lqdrl/quantum_models.py
+++ qml_lqdrl/quantum_models.py
@@ -53,8 +53,8 @@ class QuantumCritic:
                     qml.CZ(wires=[i, i+1])
                 for i in range(n_qubits):
                     qml.RY(theta[l][i], wires=i)
-            return qml.expval(qml.PauliZ(0))
-            #return [qml.expval(qml.PauliZ(i)) for i in range(n_qubits)]
+            #return qml.expval(qml.PauliZ(0))
+            return [qml.expval(qml.PauliZ(i)) for i in range(n_qubits)]
 
         self.qnode = circuit
 
@@ -69,3 +69,37 @@ class QuantumCritic:
 
     def latex(self, x):
         return qml.draw_mpl(self.qnode)(x, self.theta)
+
+    def decode_op(self, q_values, method="mean"):
+        """Decode multi-qubit outputs."""
+        q_array = qml.numpy.stack(q_values) if isinstance(q_values, (list, tuple)) else q_values
+        if method == "mean":
+            return qml.numpy.mean(q_array)
+        elif method == "sum":
+            return qml.numpy.sum(q_array)
+        else:
+            raise ValueError("Unknown decoding method")
+
+    #def decode(self, q_values, k_shots):
+
+
+    def evaluate(self, x, k_shots=10):
+        total = np.zeros(self.n_qubits)
+        for k in range(k_shots):
+            total += self.qnode(x, self.theta)
+        avg_q_per_qubit = total / k_shots
+        return avg_q_per_qubit 
+
+    '''
+    def evaluate(self, x, k_shots=10, method="mean"):
+        """Performs K-shot averaging of critic output and returns scalar."""
+        values = [self.qnode(x, self.theta) for _ in range(k_shots)]
+        stacked = qml.numpy.stack(values)  # shape (k_shots, n_qubits)
+        mean_per_qubit = qml.numpy.mean(stacked, axis=0)  # shape (n_qubits,)
+        if method == "mean":
+            return qml.numpy.mean(mean_per_qubit)  # final scalar
+        elif method == "sum":
+            return qml.numpy.sum(mean_per_qubit)
+        else:
+            raise ValueError("Unknown decoding method") 
+    '''
